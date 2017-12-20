2a2917c2a657cff7dff40402091bf3f05a353aad
Victor <victorabraham1988@gmail.com>
2017-12-20 14:03:27 -0800

    Updating BMI helper with chaining logic

--- src/aura/BMIComponent/BMIComponentHelper.js
+++ src/aura/BMIComponent/BMIComponentHelper.js
@@ -7,10 +7,33 @@
             if (state === "SUCCESS") {
                 var bmi = response.getReturnValue();
                 component.set("v.bmi", bmi);
+                this.calculateRisk(component, bmi);
             }else if (state === "ERROR") {
                 console.log(response.getError());
             }
         });
         $A.enqueueAction(action);
-	}
+	}, 
+    calculateRisk: function(component, bmi) {
+        console.log(bmi);
+        var riskAction = component.get("c.calculateRisk");
+        riskAction.setParams({bmi: bmi});
+        riskAction.setCallback(this, function(riskResponse) {
+            var riskState = riskResponse.getState();
+            if (riskState === "SUCCESS") {
+                console.log(riskResponse.getReturnValue());
+                var displayBox = component.find("display");
+                if(riskResponse.getReturnValue()) {
+                    $A.util.removeClass(displayBox, 'no-risk');
+                    $A.util.addClass(displayBox, 'is-risk');
+                }else {
+                    $A.util.removeClass(displayBox, 'is-risk');
+                    $A.util.addClass(displayBox, 'no-risk');
+                }
+            }else if (state === "ERROR") {
+                console.log(riskResponse.getError());
+            }
+        });
+        $A.enqueueAction(riskAction);
+    }
 })
